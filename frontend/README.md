e0c14d83e0c0a2d68bb54700d089755b1910a5cb
Chandra Shekhar <jmbd.group@gmail.com>
2025-02-26 07:05:57 +0530

    setup routing, page layout and header

--- frontend/README.md
+++ frontend/README.md
@@ -2,3 +2,4 @@
 
 - install react using vite
 - install and config Tailwind css
+- setup routing, page layout and header
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Landing from "./components/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <div>Hi</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/*" element={<Navigate to="/" />} />
+
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
